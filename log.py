d9e15e719dd05dbe0799b8d8deb3a5d4f35151e5
Jeremy Lyon <jeremy.lyon@thorncreek.church>
2017-09-28 11:54:39 -0600

    Update logging to append and allow for individual log files if so desired

--- log.py
+++ log.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 
 file = None
+timestamp = datetime.now().strftime('%Y%m%d-%H%M%S')
 
 def open_log():
     global file
-    file = open("e2mc.log", "w")
+    file = open("e2mc.log", "a+")
+    # Comment the above line and uncomment the line below if you want a log file
+    #   for each run of the sync
+    #file = open("e2mc.log.%s" % timestamp, "w")
 
 def close_log():
     global file
